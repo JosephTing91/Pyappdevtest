0f7b2cf90e1b763715e82efd5af170967452d392
JosephTing91 <joeksting@gmail.com>
2023-01-11 23:08:30 -0700

    add

--- main.py
+++ main.py
@@ -114,5 +114,5 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     # app.run(host='127.0.0.1', port=5000, debug=True)
---
+++ server.sh
@@ -0,0 +1,12 @@
+#user data
+
+sudo yum update -yum
+sudo yum install git -y
+sudo yum install python 3.
+
+sudo yum install docker -y
+
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+service docker start
