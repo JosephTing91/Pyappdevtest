45a03e6da3d0bc049ae6a5b9f7a38c8839028bc4
JosephTing91 <joeksting@gmail.com>
2023-01-13 19:54:38 -0700

    fixed bug

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ db = SQLAlchemy(app)
 
 ##CONFIGURE TABLE
 class BlogPost(db.Model):
-    __tablename__='postgres'
+    __tablename__='blog_post'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
